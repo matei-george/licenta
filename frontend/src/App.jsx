910114937b47ec5b843b9783d8bd3fef33cf3420
Matei <mateimurarasu@yahoo.com>
2024-04-06 13:54:08 +0300

    Add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react"
+import { Navbar } from "./Components/Navbar/Navbar"
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Navbar/>
   )
 }
