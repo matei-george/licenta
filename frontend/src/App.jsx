8e3296fd9a7854a496b137e892e8ce512497053f
Matei <mateimurarasu@yahoo.com>
2024-04-07 11:27:57 +0300

    Update main app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,20 @@
 import React from "react";
 import { Register } from "./Pages/Register/Register";
 import { Login } from "./Pages/Login/Login";
+import { Navbar } from "./Components/Navbar/Navbar";
+import { Footer } from "./Components/Footer/Footer";
+
 export default function App() {
    return (
-      <div className="flex">
-         <Register />
-         <Login />
+      <div className="flex flex-col gap-8">
+         <div className="container flex">
+            <Login />
+            <Register />
+         </div>
+         <div className="container flex flex-col">
+            <Navbar />
+            <Footer />
+         </div>
       </div>
    );
 }
